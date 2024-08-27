5ac8fc1fe475e928d12fed92873b589fc3e61626
Dylan <dylanfridman@gmail.com>
2024-08-27 11:42:51 -0400

    Updated intro section

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,15 @@ export default function Page() {
         Dylan Fridman
       </h1>
       <p className="mb-4">
-        {`I'm currently a Masters student in Computer Science at Dartmouth College.
-        I did my undergraduate in Mathematics.`}
+        {`I'm currently a Masters student in Computer Science at Dartmouth College. In the past,
+        I've worked on computational topology and on mathematical formalization in Lean.
+        Although I did my undergraduate in Mathematics, I'm moving away from pure math and towards
+        more applied areas of computer science since I believe that's how I can make the most impact.`}
       </p>
-      <p className="mb-4">
+      <p>
         {`Take a look at my `}
         <StyledLink inParagraph href="/cv.pdf">CV</StyledLink>
         .
-      </p>
-      <p>
-        {`Besides coding, I like to climb.`}
       </p>
     </section>
   )
