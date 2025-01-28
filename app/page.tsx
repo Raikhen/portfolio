323dbc30504e88036235824dfdfc552c91d815c1
Dylan <dylanfridman@gmail.com>
2025-01-28 11:27:02 -0500

    Update home text

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,30 @@ export default function Page() {
         Dylan Fridman
       </h1>
       <div className="grid grid-cols-3 space-x-8">
-        <p className="mb-4 col-span-2">
-          {`I'm currently a Masters student in Computer Science at Dartmouth College. In the past,
+        <div className="col-span-2 flex flex-col gap-6">
+          <p>
+            I'm currently a MS student in Computer Science at Dartmouth College.
+          </p>
+          <p>
+            Although I dedicated most of my adult life to the pursuit of a career in
+            Mathematics and Computer Science Theory, I'm now looking for more direct ways to
+            have a positive impact than through academia. As of right now, I'm aiming towards
+            earning to give as either a software developer or quantitative trader.
+          </p>
+          <p>
+            Whenever I'm not working, you can find me climbing mountains or training to do so.
+          </p>
+          {/*`I'm currently a Masters student in Computer Science at Dartmouth College. In the past,
           I've worked on computational topology and on mathematical formalization in Lean.
           Although I did my undergraduate in Mathematics, I'm moving away from pure math and towards
-          more applied areas of computer science since I believe that's how I can make the most impact.`}
-        </p>
+          more applied areas of computer science since I believe that's how I can make the most impact.`*/}
+        </div>
         <div className="col-span-1">
           <Image
             src="/images/dylan.jpg"
             alt="Dylan Fridman"
             width={1000}
-            height={10000}
+            height={300}
             className="rounded"
           />
         </div>
