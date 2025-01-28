9d38ce5ebdbf166023451db84ade5271adbd6b6e
Dylan <dylanfridman@gmail.com>
2025-01-27 20:45:51 -0500

    Make the date format match across pages

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -17,10 +17,9 @@ export function BlogPosts() {
           return 1
         })
         .map((post) => (
-          <Link
+          <div
             key={post.slug}
             className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400 w-[160px] tabular-nums">
@@ -30,7 +29,7 @@ export function BlogPosts() {
                 {post.metadata.title}
               </StyledLink>
             </div>
-          </Link>
+          </div>
         ))}
     </div>
   )
